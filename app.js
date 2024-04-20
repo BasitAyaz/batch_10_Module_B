38af016dc218c6d3c146e53efbf3411fc742215e
BasitAyaz <basitahmed1997@gmail.com>
2024-04-20 17:36:35 +0500

    20-4-2024

---
+++ app.js
@@ -0,0 +1,149 @@
+// var a = 1; true
+// var a = 0; false
+// var a = -1; true
+// var a = true;
+// var a = false;
+// var a = undefined;
+// var a = ""; //false
+// var a = " "; //true
+// var a = "0"; //true
+// var a = NaN; //false
+// var a = []; //true
+// var a = {}; //true
+// var a = null; //false
+
+// if (a) {
+//   console.log("If Scope Run");
+// }
+
+// var arr = [];
+// arr[0] = 123;
+
+// var obj = {
+//   name: "ABC",
+//   age: 18,
+// };
+// // obj.id = 20;
+// obj["id"] = 20;
+
+// console.log(obj);
+// console.log(arr);
+
+// array and object  (object)
+
+// create a searching mechanism for search mobile in given data.
+
+// var mobiles = {
+//   iphone: {
+//     iphone7: {
+//       RAM: "",
+//       ROM: "",
+//       Camera: "",
+//       LCD: "",
+//     },
+//     iphone8: {},
+//     iphone8plus: {},
+//     iphoneX: {},
+//     iphone11: {},
+//   },
+//   xiomi: {
+//     redmiA2: {},
+//     redmi10: {},
+//     redmi11: {},
+//     redmi12: {},
+//     redmi13: {},
+//     redmi13C: {},
+//   },
+//   realme: {
+//     realmiNote50: {},
+//     realmiC67: {},
+//     realmiC53: {},
+//     realmi9: {},
+//     realmi9i: {},
+//   },
+//   tecno: {
+//     spark20: {},
+//     spark20Pro: {},
+//     spark20C: {},
+//     POVA6: {},
+//     PhantomV: {},
+//   },
+// };
+
+// var brand = document.getElementById("brand");
+// var model = document.getElementById("model");
+
+// var allBrands = Object.keys(mobiles);
+// for (var i = 0; i < allBrands.length; i++) {
+//   brand.innerHTML += `<option>${allBrands[i]}</option>`;
+// }
+
+// function selectBrand() {
+//   var selectedBrand = brand.value;
+//   var allModels = Object.keys(mobiles[selectedBrand]);
+//   model.innerHTML = "";
+//   for (var i = 0; i < allModels.length; i++) {
+//     model.innerHTML += `<option value="${allModels[i]}">${allModels[i]}</option>`;
+//   }
+// }
+
+// function selectModel() {
+//   console.log(brand.value, model.value);
+//   console.log(mobiles[brand.value][model.value]);
+// }
+
+// var
+// reinitialize (y)
+// reassign (y)
+// hoisting (y)
+// blockscope (y)
+
+// let
+// reinitialize (n)
+// reassign (y)
+// hoisting (n)
+// blockscope (y)
+
+// const
+// reinitialize (n)
+// reassign (n)
+// hoisting (n)
+// blockscope (y)
+
+// ======= Template Literals ======
+
+// var a = 20;
+
+// console.log(`squre of ${a} is ${a * a}`);
+
+// ======= Ternary Operators ======
+
+// let studentCard = false;
+// let age = 16;
+
+// if (age >= 18) {
+//   console.log("allow");
+// } else if (studentCard) {
+//   console.log("Allow on Student Card");
+// } else {
+//   console.log("Allow Allow");
+// }
+
+// age >= 18
+//   ? console.log("Allow")
+//   : studentCard
+//   ? console.log("Allow on Student Card")
+//   : console.log("Not Allow");
+
+// age >= 18 || studentCard ? console.log("Allow") : console.log("Not Allow");
+
+// let status = age >= 18 ? true : false;
+
+// console.log(status);
+
+// ======= Ternary Operators ======
+let age = 0;
+let studentCard = true;
+
+let status = age && studentCard;
+console.log(status);
